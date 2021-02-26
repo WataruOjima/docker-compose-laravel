c140729da04add30e21fe906899a5296aeb53026
WataruOjima <wataru.ojima20@gmail.com>
2021-02-26 23:46:31 +0900

    UsersTableSeeder.php編集

--- my-laravel-app/database/seeds/UsersTableSeeder.php
+++ my-laravel-app/database/seeds/UsersTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\User; 
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,23 +12,24 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = [
-            [
-                'name'      => 'admin',
-                'email'     => 'admin@example.com',
-                'password'  => Hash::make('password'),
-                'admin_flg' => true,
-            ],
-            [
-                'name'      => 'test0001',
-                'email'     => 'test0001@example.com',
-                'password'  => Hash::make('password'),
-                'admin_flg' => false,
-            ],
-        };
-        
-          // 登録
-          foreach ($users as $user) {
-          User::create($user);
+      $users = [
+        [
+          'name'      => 'admin',
+          'email'     => 'admin@example.com',
+          'password'  => Hash::make('password'),
+          'admin_flg' => true,
+        ],
+        [
+          'name'      => 'test0001',
+          'email'     => 'test0001@example.com',
+          'password'  => Hash::make('password'),
+          'admin_flg' => false,
+        ],
+      ];
+    
+      // 登録
+      foreach ($users as $user) {
+        User::create($user);
+      }
     }
-}
+    
