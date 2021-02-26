a65be5a96018344c83fe76982fc518316cb5275e
WataruOjima <wataru.ojima20@gmail.com>
2021-02-26 23:09:45 +0900

    MicropostController.php追記

--- my-laravel-app/app/Http/Controllers/MicropostController.php
+++ my-laravel-app/app/Http/Controllers/MicropostController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class MicropostController extends Controller
 {
-    //
+    /** 
+     * 投稿一覧表示アクション
+     */
+    public function index()
+    {
+        return view('micropost.index');
+    }
+
 }
