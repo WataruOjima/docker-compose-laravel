c228a2d4b9e8c91b3f4be0999ccbeb7cf0fd39d2
WataruOjima <wataru.ojima20@gmail.com>
2021-02-26 14:25:25 +0900

    UserController.phpに追加

--- my-laravel-app/app/Http/Controllers/UserController.php
+++ my-laravel-app/app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Auth;
 class UserController extends Controller
 {
     /**
@@ -13,5 +13,15 @@ class UserController extends Controller
     {
         return view('user.signin');
     }
+    public function login(Request $request)
+    {
+        $email    = $request->input('email');
+        $password = $request->input('password');
+        if (!Auth::attempt(['email' => $email, 'password' => $password])) {
+            // 認証失敗
+            return redirect('/')->with('error_message', 'I failed to login');
+        }
+        // 認証成功
+        return redirect()->route('micropost.index');
+    }
 }
-
