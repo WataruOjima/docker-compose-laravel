7482074f960ea34fb0e47b030f9b1abb8a71d53e
WataruOjima <wataru.ojima20@gmail.com>
2021-02-28 23:14:23 +0900

    Policies/UserPolicy.php編集
    viewメソッドとupdateメソッド以外は削除

--- my-laravel-app/app/Policies/UserPolicy.php
+++ my-laravel-app/app/Policies/UserPolicy.php
@@ -4,21 +4,11 @@ namespace App\Policies;
 
 use App\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Auth;
 
 class UserPolicy
 {
     use HandlesAuthorization;
-
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
 
     /**
      * Determine whether the user can view the model.
@@ -29,18 +19,7 @@ class UserPolicy
      */
     public function view(User $user, User $model)
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
+        return $model->id == $user->id;
     }
 
     /**
@@ -52,42 +31,6 @@ class UserPolicy
      */
     public function update(User $user, User $model)
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\User  $model
-     * @return mixed
-     */
-    public function delete(User $user, User $model)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\User  $model
-     * @return mixed
-     */
-    public function restore(User $user, User $model)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\User  $model
-     * @return mixed
-     */
-    public function forceDelete(User $user, User $model)
-    {
-        //
+        return $model->id == $user->id;
     }
 }
