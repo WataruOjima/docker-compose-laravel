24f1b66a52e85c584b2d184cca27b7e8097f53e4
WataruOjima <wataru.ojima20@gmail.com>
2021-03-01 13:03:21 +0900

    MicropostController.phpにpostアクションを追加
    $micropost->micropostSave($params)
    Micropostモデルのメソッドを呼び出して、投稿内容をDBに登録

--- my-laravel-app/app/Http/Controllers/MicropostController.php
+++ my-laravel-app/app/Http/Controllers/MicropostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Micropost; 
+use Auth;
 
 use Illuminate\Http\Request;
 
@@ -26,4 +27,22 @@ class MicropostController extends Controller
       {
           return view('micropost.input');
       }
+
+      /**
+       * 投稿処理アクション
+       */
+      public function post(Request $request)
+      {
+          $content      =  $request->input('content');
+          $micropost    =  new Micropost;
+          $params = [
+              'user_id' =>  Auth::id(),
+              'content' =>  $content,
+          ];
+          if(!$micropost->micropostSave($params)) {
+              // 登録失敗
+              return redirect()->route('micropost.input')->with('error_message', 'Regist micropost failed');
+          }
+          return redirect()->route('micropost.index')->with('flash_message', 'Regist success!!');
+     }
 }
