c7d1c4fbebdd2f99b090183ba154b7b5f82bb09d
WataruOjima <wataru.ojima20@gmail.com>
2021-03-02 00:35:03 +0900

    編集５

--- my-laravel-app/app/Providers/AppServiceProvider.php
+++ my-laravel-app/app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,9 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
-        }
+
     }
 }
