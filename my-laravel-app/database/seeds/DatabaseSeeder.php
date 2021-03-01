3c21f8e208ff85157daadc8b1399e84cae0cc7d8
WataruOjima <wataru.ojima20@gmail.com>
2021-03-01 11:54:53 +0900

    DatabaseSeeder.php訂正

--- my-laravel-app/database/seeds/DatabaseSeeder.php
+++ my-laravel-app/database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersTableSeeder::class,
-            micropostsTableSeeder::class,
+            MicropostsTableSeeder::class,
         ]);
     }
 }
