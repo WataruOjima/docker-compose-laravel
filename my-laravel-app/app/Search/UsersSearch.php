50e53fe7ec0ef75f1867c89c007b087b162da043
WataruOjima <wataru.ojima20@gmail.com>
2021-03-03 11:13:40 +0900

    検索用の画面を作成2

--- my-laravel-app/app/Search/UsersSearch.php
+++ my-laravel-app/app/Search/UsersSearch.php
@@ -8,7 +8,14 @@ class UsersSearch extends Searchable
     public function __construct()
     {
         $this->params = [
-            //
+            // ここに検索条件を記載します
+            // サンプルでは名前 と 住所を検索できるようにします
+            'name' => [
+                'type' => 'value'
+            ],
+            'address' => [
+                'type' => 'like'
+            ],
         ];
     }
 }
