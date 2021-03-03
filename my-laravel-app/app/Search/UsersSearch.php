1765fd5431c4882b19b7539b7d76350d014f37ee
WataruOjima <wataru.ojima20@gmail.com>
2021-03-03 11:08:49 +0900

    Searchable、検索の実態を記載するクラス作成

---
+++ my-laravel-app/app/Search/UsersSearch.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Search;
+
+use Search\Searchable;
+
+class UsersSearch extends Searchable
+{
+    public function __construct()
+    {
+        $this->params = [
+            //
+        ];
+    }
+}
